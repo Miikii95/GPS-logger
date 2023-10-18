b6264be902a2e68471b9075c944a793fe6d83f53
Miikii95 <Mikuleckrzysztof@gmail.com>
2023-10-18 18:39:42 +0200

    Add project files.

---
+++ MapForLogger.03/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace MapForLogger._03
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.mapControl = new System.Windows.Forms.MapControl();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // mapControl
+            // 
+            this.mapControl.Cursor = System.Windows.Forms.Cursors.Cross;
+            this.mapControl.Dock = System.Windows.Forms.DockStyle.Left;
+            this.mapControl.ErrorColor = System.Drawing.Color.Red;
+            this.mapControl.FitToBounds = true;
+            this.mapControl.Location = new System.Drawing.Point(0, 0);
+            this.mapControl.Name = "mapControl";
+            this.mapControl.ShowThumbnails = true;
+            this.mapControl.Size = new System.Drawing.Size(952, 601);
+            this.mapControl.TabIndex = 0;
+            this.mapControl.Text = "mapControl1";
+            this.mapControl.ThumbnailBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.mapControl.ThumbnailForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(176)))), ((int)(((byte)(176)))), ((int)(((byte)(176)))));
+            this.mapControl.ThumbnailText = "Downloading...";
+            this.mapControl.TileImageAttributes = null;
+            this.mapControl.ZoomLevel = 0;
+            this.mapControl.Click += new System.EventHandler(this.mapControl_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Right;
+            this.panel1.Location = new System.Drawing.Point(958, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(200, 601);
+            this.panel1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(19, 30);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 28);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(120F, 120F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.ClientSize = new System.Drawing.Size(1158, 601);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.mapControl);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MapControl mapControl;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ MapForLogger.03/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MapForLogger._03
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            mapControl.CacheFolder = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "MapControl");
+            string userAgent = "My non commercial app using for WinFormsMapControl";
+            mapControl.TileServer = new OpenStreetMapTileServer(userAgent);
+            mapControl.ZoomLevel = 12;
+            
+
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            mapControl.Center = new GeoPoint(19.93f,50.06f);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var center = mapControl.Center;
+            float lat = center.Latitude;
+            float lon = center.Longitude;
+            MessageBox.Show($"lat = {lat} | lon = {lon}");
+            
+        }
+
+        private void mapControl_Click(object sender, EventArgs e)
+        {
+           
+        }
+    }
+}
